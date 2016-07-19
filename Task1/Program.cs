e6151bf38a81e1214a7bea74f33d8146b169dc4e
VolhaShyrko <olgashyrko@gmail.com>
2016-07-19 09:57:40 +0300

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Task1
+{
+    class Program
+    {
+        //Попытайтесь разорвать связь между классами через InfoData (класс Destination ничего не должен знать об этом типе)
+        static void Main(string[] args)
+        {
+        }
+        struct InfoData
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+
+        class Source
+        {
+            internal void CheckAndProceed(InfoData[] data)
+            {
+                var dest = new Destination();
+
+                //do something
+
+                dest.ProceedData(data);
+            }
+        }
+
+        class Destination
+        {
+            public void ProceedData<T>(T[] data) where T : struct
+            {
+                for (var i = 0; i < data.Length; i++)
+                {
+                    //do sth
+                }
+            }
+        }
+    }
+}
